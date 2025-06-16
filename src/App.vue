4fa3d20424ca4e1853e12c88e7272227e85013ec
omarahorrar <omarahorar@gmail.com>
2025-06-16 10:01:07 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="background">
+    <Navbar />
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="navbar">
+    <img :src="require('@/assets/logo.png')" alt="Logo" class="logo" />
+    <div class="nav-links">
+      <a href="#hero">Home</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+    </div>
+  </nav>
+</template>
